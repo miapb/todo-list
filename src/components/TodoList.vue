cbd05d7c8a845b99ff325122a9fe4de76437625c
Unknown <miapinberge@gmail.com>
2019-03-29 02:21:38 +0100

    Replace HelloWorld boiler plate with initial Todo list

---
+++ src/components/TodoList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <h1>{{ name }}'s To-Do list</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Mia"
+    };
+  },
+  methods() {},
+  computed() {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+$palette_regular: rgb(255, 0, 0);
+$palette_hightlight: rgb(0, 47+20, 255);
+
+h1 {
+  color: $palette_hightlight;
+  &:hover {
+    color: $palette_regular;
+  }
+}
+</style>
